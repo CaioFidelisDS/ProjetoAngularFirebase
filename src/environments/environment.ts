fc831961e5d477779c2841dfb3783f50b3b54518
CaioFidelisDS <125466050+CaioFidelisDS@users.noreply.github.com>
2025-02-19 21:06:10 +0000

    Atualização home page

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,13 +4,13 @@
 
 export const environment = {
   firebaseConfig: {
-    apiKey: "AIzaSyCGdwCWx_8o2hYEmLBhK6xQzCMp5h12gzk",
-    authDomain: "aula-3e82a.firebaseapp.com",
-    projectId: "aula-3e82a",
-    storageBucket: "aula-3e82a.firebasestorage.app",
-    messagingSenderId: "258284807710",
-    appId: "1:258284807710:web:72b06b67cea8d15cd6b00f",
-    measurementId: "G-X8L8GS7897"
+  apiKey: "AIzaSyBwzcZR76semi0DzFApTTpXti9LzQ3WBD8",
+  authDomain: "aula1-7fb7f.firebaseapp.com",
+  projectId: "aula1-7fb7f",
+  storageBucket: "aula1-7fb7f.firebasestorage.app",
+  messagingSenderId: "541777579082",
+  appId: "1:541777579082:web:d1a51332ff3d4e90ec2ab3",
+  measurementId: "G-LEM6QRNSXH"
   },
   production: false
 };
